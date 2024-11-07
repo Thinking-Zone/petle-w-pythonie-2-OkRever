9bbd04c3b55d874ecd2b247cc91e33fc5a196bc5
OkRever <Kazik.bochinski@gmail.com>
2024-11-07 14:17:25 +0100

    Update zadanie6.py

--- zadanie6.py
+++ zadanie6.py
@@ -1,2 +1,2 @@
-#while używane jest kiedy nie wiemy ile razy chcemy cos powtórzyć
+#while używane jest kiedy nie wiemy ile razy chcemy cos powtórzyć i będzie działać aż coś co ustaliliśy nie zatrzyma 
 #for używane jest wtedy kiedy wiemy ile razy dokładnie chcemy powtórzeń
